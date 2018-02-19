6b67657dbbeacd33f628f49ec7a49c3e261d1d51
NHSDev <Nick_Struik@outlook.com>
2018-02-19 15:22:33 +0100

    Add Main Menu UI

---
+++ ProjectStudio_BHV/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public GameObject Menu;
+    public GameObject Credits;
+
+    void Start () {
+        Credits.SetActive(false);
+    }
+
+    public void LoadGame() {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Quit() {
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+                Application.Quit ();
+    #endif
+    }
+
+    public void LoadMainMenu() {
+        Menu.SetActive(true);
+        Credits.SetActive(false);
+    }
+
+    public void LoadCredits()
+    {
+        Menu.SetActive(false);
+        Credits.SetActive(true);
+    }
+}
