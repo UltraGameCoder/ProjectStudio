5a914d5b632e7912b55d99062bc0a0bca1dc627a
MCanC <ghasrany@gmail.com>
2018-02-20 15:03:49 +0100

    Add playercontroller

---
+++ ProjectStudio_BHV/Assets/Scripts/Contoll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Contoll : MonoBehaviour {
+
+    public int rotateSpeed;
+    public int walkSpeed;
+
+	void Start () {
+		
+	}
+
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * rotateSpeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * walkSpeed;
+        
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
