fda9ecc6873d8c355ebd628149818387c1e9d75e
Emiel Maggi <emiel.maggi.010@gmail.com>
2018-02-22 15:14:00 +0100

    Raycast HurtPeople

---
+++ ProjectStudio_BHV/Assets/Scripts/Raycasting.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycasting : MonoBehaviour {
+
+   public Camera cam;
+    public GameObject infoPanel;
+
+
+	// Use this for initialization
+	void Start () {
+        infoPanel.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.CompareTag("HurtPerson"))
+                {
+                    infoPanel.SetActive(true);
+                }
+                else
+                {
+                    infoPanel.SetActive(false);
+                }
+                print("I'm looking at " + hit.transform.name);
+            }
+            else
+            {
+                infoPanel.SetActive(false);
+                print("I'm looking at nothing!");
+            }
+        }
+    }
+}
