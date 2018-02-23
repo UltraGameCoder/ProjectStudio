c246c8a94d379ab18b53aab2dd0044d3902cb86d
MCanC <ghasrany@gmail.com>
2018-02-23 14:40:35 +0100

    Mouse support

---
+++ ProjectStudio_BHV/Assets/Scripts/Control.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+
+    public int walkSpeed;
+
+    public float hSpeed = 2.0f;
+    public float vSpeed = 2.0f;
+
+    private float hSight = 0.0f;
+    private float vSight = 0.0f;
+
+    void Update()
+    {
+  
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * Time.deltaTime * walkSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * Time.deltaTime * walkSpeed;
+        }
+  
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        hSight += hSpeed * Input.GetAxis("Mouse X");
+        vSight -= vSpeed * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(vSight, hSight, 0.0f);
+
+    }
+}
