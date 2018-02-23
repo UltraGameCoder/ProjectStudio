d50d48fa50f4be51f4fb854e1ca9b955d061b97b
Emiel Maggi <emiel.maggi.010@gmail.com>
2018-02-23 14:38:08 +0100

    Add SituationScript

--- ProjectStudio_BHV/Assets/Scripts/Raycasting.cs
+++ ProjectStudio_BHV/Assets/Scripts/Raycasting.cs
@@ -19,7 +19,7 @@ public class Raycasting : MonoBehaviour {
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hit, 3))
             {
                 if (hit.transform.CompareTag("HurtPerson"))
                 {
---
+++ ProjectStudio_BHV/Assets/Scripts/SituationPanelScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SituationPanelScript : MonoBehaviour {
+
+    public Text question;
+    public Text[] answers;
+
+	// Use this for initialization
+	void Start () {
+        question.text = "Vraag";
+        answers[0].text = "Antwoord 1";
+        answers[1].text = "Antwoord 2";
+        answers[2].text = "Antwoord 3";
+        answers[3].text = "Antwoord 4";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
